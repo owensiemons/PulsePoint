9ff57febfb2cf0683c08d154c84f098f3330a046
Owen <osiemons@gmail.com>
2025-09-10 08:30:28 -0500

    Rebuilt and redeployed.

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+export default function App() {
+  return (
+    <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <SpinningCube />
+      <OrbitControls />
+    </Canvas>
+  );
+}
+
+function SpinningCube() {
+  const meshRef = useRef();
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
